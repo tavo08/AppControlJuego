0b713a5aecc1010cc292ac7b43c875b8e9786823
Eduardo Fernandez <eduardo.fernandez@accesso.com>
2019-06-28 21:50:26 -0500

    Refactor MainActivity to Activity package

--- app/src/main/java/com/tavo/appcontroljuego/activity/MainActivity.kt
+++ app/src/main/java/com/tavo/appcontroljuego/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tavo.appcontroljuego
+package com.tavo.appcontroljuego.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tavo.appcontroljuego.R
 
 class MainActivity : AppCompatActivity() {
 
